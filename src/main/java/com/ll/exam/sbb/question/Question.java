c540065e38dec4a1192c4d4073b5e3df390cc87c
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-08-23 13:43:04 +0900

    Feat : 추천 기능

--- src/main/java/com/ll/exam/sbb/question/Question.java
+++ src/main/java/com/ll/exam/sbb/question/Question.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -35,6 +36,9 @@ public class Question {
     private SiteUser author;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 
     public void addAnswer(Answer answer) {
         answer.setQuestion(this); // 해당하는 Answer의 Question부분에 이 Question 객체를 저장한다.
