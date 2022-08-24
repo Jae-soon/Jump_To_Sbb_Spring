b55837c3b8d939d06468bf339d5bbb7428829237
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-08-24 15:25:06 +0900

    질문 제목, 내용, 작성자, 답변내용으로 검색

--- src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
@@ -26,4 +26,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>, Re
     Page<Question> findBySubjectContainsOrContentContains(String kw, String kw_, Pageable pageable);
 
     Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContains(String kw, String kw_, String kw__, Pageable pageable);
+    Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
 }
